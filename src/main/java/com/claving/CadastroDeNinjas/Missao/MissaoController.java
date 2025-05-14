13ac17ce03cc18d320ca4f6102df983c62f008d5
samuel santos <sam.santo2209@gmail.com>
2025-05-14 09:54:36 -0300

    update

--- src/main/java/com/claving/CadastroDeNinjas/Missao/MissaoController.java
+++ src/main/java/com/claving/CadastroDeNinjas/Missao/MissaoController.java
@@ -1,5 +1,4 @@
 package com.claving.CadastroDeNinjas.Missao;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/claving/CadastroDeNinjas/Missao/MissoesModel.java
+++ src/main/java/com/claving/CadastroDeNinjas/Missao/MissoesModel.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class MissoesModel {
-
+git
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
